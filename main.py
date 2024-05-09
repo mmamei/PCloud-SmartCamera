9fd435233a14cd1f9c7e04a7339f7cc5b2e143bd
mmamei <marco.mamei@unimore.it>
2024-05-09 12:23:04 +0200

    first commit

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from google.cloud import storage
 import json
 from datetime import datetime
 import os
+from google.cloud import vision
 
 class User(UserMixin):
     def __init__(self, username):
@@ -37,12 +38,12 @@ def main():
 @app.route('/login', methods=['POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('/sensors'))
+        return redirect(url_for('/'))
     username = request.values['u']
     password = request.values['p']
     if username in usersdb and password == usersdb[username]:
         login_user(User(username))
-        return redirect('/sensors')
+        return redirect('/')
     return redirect('/static/login.html')
 
 @app.route('/logout')
@@ -56,17 +57,12 @@ def logout():
 def get_camera():
     return redirect('/static/camera.html')
 
+db_client = 'sensors'
+coll = 'camera'
 
-@app.route('/cam',methods=['GET'])
-def cam():
-    return 'ciao'
-
-
-db = 'sensors'
-coll = 'data'
-db = firestore.Client.from_service_account_json('credentials.json', database=db)
+db_client = firestore.Client.from_service_account_json('credentials.json', database=db_client)
 storage_client = storage.Client.from_service_account_json('credentials.json')
-
+vision_client = vision.ImageAnnotatorClient.from_service_account_json('credentials.json')
 
 @app.route('/upload/<camera>',methods=['POST'])
 def upload(camera):
@@ -77,22 +73,55 @@ def upload(camera):
     bucket = storage_client.bucket('upload-smart-camera')
     blob = bucket.blob(f'{camera}-{current_time}')
     blob.upload_from_string(file.read(), content_type=file.content_type)
+
+    image = vision.Image()
+    image.source.image_uri = f'gs://upload-smart-camera/{camera}-{current_time}'
+    response = vision_client.face_detection(image=image)
+    faces = response.face_annotations
+    print('Faces', len(faces))
+
+    id = f'{camera}-{current_time}'
+    doc_ref = db_client.collection(coll).document(id)
+    doc_ref.set({'cam': camera, 'date':current_time, 'num':len(faces)})
+    #print(doc_ref.get().id)
+
     return 'saved'
 
-
 @login_required
-@app.route('/frames/<camera>',methods=['GET'])
-def frames(camera):
+@app.route('/frames/<camera>/<date>',methods=['GET'])
+def frames(camera, date):
 
-    blobs = storage_client.list_blobs('upload-smart-camera', prefix=camera)
-    blobs = [b.name for b in blobs]
-    print(blobs)
-    fname = blobs[-1]
+    #blobs = storage_client.list_blobs('upload-smart-camera', prefix=camera)
+    #blobs = [b.name for b in blobs]
+    #print(blobs)
+    # cam1-2024_05_09_11_37_50
+    fname = f'{camera}-{date}'
     bucket = storage_client.bucket('upload-smart-camera')
     blob = bucket.blob(fname)
     blob.download_to_filename(f'/tmp/{fname}.png')
     return send_file(f'/tmp/{fname}.png')
 
+
+
+@app.route('/cam_data/<cam>',methods=['GET'])
+def get_data(cam):
+    r = []
+    for doc in db_client.collection(coll).where('cam', '==', cam).stream():
+        print(f'{doc.id} --> {doc.to_dict()}')
+        r.append([doc.to_dict()['date'],doc.to_dict()['num']])
+    return json.dumps(r)
+
+@app.route('/graph/<cam>', methods=['GET'])
+def graph(cam):
+    print('ciao2')
+    d2 = json.loads(get_data(cam))
+    ds = '' # ['2004',  1000,      null],
+
+    for date,val in d2:
+        ds += f"['{date}','/frames/{cam}/{date}',{val}],\n"
+
+    print(ds)
+    return render_template('graph.html',data=ds)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=222, debug=True,ssl_context='adhoc')
--- test-google-image.py
+++ test-google-image.py
@@ -4,16 +4,14 @@ from google.cloud import vision
 # Instantiates a client
 client = vision.ImageAnnotatorClient.from_service_account_json('credentials.json')
 # The name of the image file to annotate
-file_name = 'tmp/PXL_20231203_065456467.jpg'
-
+#file_name = 'tmp/PXL_20231203_065456467.jpg'
 # Loads the image into memory
-with io.open(file_name, 'rb') as image_file:
-    content = image_file.read()
-
+#with io.open(file_name, 'rb') as image_file:
+#    content = image_file.read()
 #image = vision.Image(content=content)
 
 image = vision.Image()
-image.source.image_uri = 'gs://upload-smart-camera/cam1-14_20_07'
+image.source.image_uri = 'gs://upload-smart-camera/cam1-2024_05_09_09_07_51'
 response = client.face_detection(image=image)
 faces = response.face_annotations
 print('Faces',len(faces))
